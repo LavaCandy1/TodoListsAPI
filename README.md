10951b8c705167219d51cc23f0dcd72d7839f354
LavaCandy1 <aayush.garg.1793@gmail.com>
2024-09-17 13:09:57 +0530

    README.md upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# FastAPI To-Do Application
+
+This is a simple To-Do list application built using FastAPI and PostgreSQL as the backend database. The application allows users to add, delete, and mark tasks as completed directly from the web interface built using Jinja2 templates.
+
+## Features
+
+    - Add new to-do items
+    - Mark tasks as completed or pending
+    - Delete tasks
+    - PostgreSQL integration for persistent data storage
+    - Dynamic HTML rendering using Jinja2 templates
+
+## Tech Stack
+
+    - FastAPI: For creating the web server and API.
+    - PostgreSQL: Database for storing to-do items.
+    - SQLAlchemy: ORM used to interact with PostgreSQL.
+    - Jinja2: Templating engine for rendering dynamic HTML.
+    - HTML & CSS: Frontend styling and structure.
--- main.py
+++ main.py
@@ -35,7 +35,6 @@ async def put_name(req : Request,db:db_dependency):
     result = db.query(models.Todo).all()
     return templates.TemplateResponse("todoHome.html", {"request": req , "name" : "World!", "todoList" : result})
 
-
 @todoApp.post("/add-todoHTML")
 async def add_todo(db : db_dependency, todo_textHTML : str=Form(...)):
     todo = TodoBase(todo_text=todo_textHTML)
@@ -43,16 +42,6 @@ async def add_todo(db : db_dependency, todo_textHTML : str=Form(...)):
     db.add(db_todo)
     db.commit()
     return RedirectResponse(url="/todoHome",status_code=status.HTTP_303_SEE_OTHER)
-
-@todoApp.delete("/delete-todo/{todo_id}")
-async def del_todo(todo_id : int, db : db_dependency ):
-    to_delete = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
-    if not to_delete:
-        raise HTTPException(status_code=404, detail=f"Todo id {todo_id} not found")
-    db.delete(to_delete)
-    db.commit()
-    db.refresh(to_delete)
-
 
 @todoApp.get("/delete-todo/{todo_id}")
 async def del_todo(todo_id : int, db : db_dependency ):
@@ -62,7 +51,6 @@ async def del_todo(todo_id : int, db : db_dependency ):
     db.delete(to_delete)
     db.commit()
     return RedirectResponse(url="/todoHome",status_code=status.HTTP_303_SEE_OTHER)
-
 
 @todoApp.get("/toggle-completion/{todo_id}")
 async def update_todo(todo_id : int, db : db_dependency):
@@ -92,3 +80,13 @@ async def update_todo(todo_id : int, db : db_dependency):
 #     if not result:
 #         raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found!")
 #     return result
+
+
+# @todoApp.delete("/delete-todo/{todo_id}")
+# async def del_todo(todo_id : int, db : db_dependency ):
+#     to_delete = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+#     if not to_delete:
+#         raise HTTPException(status_code=404, detail=f"Todo id {todo_id} not found")
+#     db.delete(to_delete)
+#     db.commit()
+#     db.refresh(to_delete)
