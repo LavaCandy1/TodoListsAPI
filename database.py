535fccf862d31aa510d70d355e2683f334d593e7
LavaCandy1 <aayush.garg.1793@gmail.com>
2024-09-21 11:11:08 +0530

    Final update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_Database = "postgresql://postgres:root@localhost:5432/TodoData"
+URL_Database = "postgresql://<username>:<password>@localhost:5432/TodoData"
 
 engine = create_engine(URL_Database)
 
